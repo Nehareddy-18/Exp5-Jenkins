b13f64776dd1eb963dad2225d593592d58d9b240
nehareddy <24d21a05p7@gmail.com>
2026-09-10 22:45:01 +0530

    Add Samplename.java

---
+++ Samplename.java
@@ -0,0 +1,9 @@
+public class Samplename {
+    public static void main(String[] args) {
+        // Get user name
+        String name = "John Doe";
+
+        // print the name to the console
+        System.out.println("Hello, my name is " + name);
+    }
+}
