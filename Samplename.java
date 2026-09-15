4a32fd74b76fd433586fc4696042361f52742d3a
nehareddy <24d21a05p7@gmail.com>
2026-09-15 23:01:02 +0530

    Update Samplename

--- Samplename.java
+++ Samplename.java
@@ -1,7 +1,7 @@
 public class Samplename {
     public static void main(String[] args) {
         // Get user name
-        String name = "John Doe";
+        String name = "Neha Reddy";
 
         // print the name to the console
         System.out.println("Hello, my name is " + name);
